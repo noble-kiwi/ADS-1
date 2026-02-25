fece52bfed0b7f59c5e2b06206f230e4f54a789f
noble_kiwi <iwkr1208@gmail.com>
2026-02-25 20:04:31 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,40 +7,40 @@ bool checkPrime(uint64_t value) {
     if (value < 2) {
         return false;
     }
-    if (value == 2 || value == 3) {
+    else if (value == 2 || value == 3) {
         return true;
     }
-    if (value % 2 == 0 || value % 3 == 0) {
+    else if (value % 2 == 0 || value % 3 == 0) {
         return false;
     }
-    for (uint64_t i = 5; i * i <= value; i += 6) {
-        if (value % i == 0 || value % (i + 2) == 0) {
-            return false;
+    else {
+        for (int i = 4; i < (value/2)+1; i++) {
+            if (value % i == 0) {
+                return false;
+            }
         }
     }
     return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    if (n == 0) {
-        return 0;
-    }
+    if (n == 0) { return 0; }
+    uint64_t number = 0;
     uint64_t count = 0;
-    uint64_t number = 1;
-    while (true) {
-        number++;
+    while (1) {
         if (checkPrime(number)) {
             count++;
             if (count == n) {
                 return number;
             }
         }
+        number++;
     }
 }
 
 uint64_t nextPrime(uint64_t value) {
     uint64_t number = value + 1;
-    while (true) {
+    while (1) {
         if (checkPrime(number)) {
             return number;
         }
@@ -52,7 +52,7 @@ uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; i++) {
         if (checkPrime(i)) {
-            sum += i;
+            sum = sum + i;
         }
     }
     return sum;
@@ -60,7 +60,7 @@ uint64_t sumPrime(uint64_t hbound) {
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
     uint64_t count = 0;
-    for (uint64_t i = lbound; i + 2 < hbound; i++) {
+    for (uint64_t i = lbound; i < hbound-2; i++) {
         if (checkPrime(i) && checkPrime(i + 2)) {
             count++;
         }
